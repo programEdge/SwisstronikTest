76b342327da13ac3bdf04890acc32e606fcb10ad
hyh <hyh@digital.com>
2024-08-19 23:00:02 +0800

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,35 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+import { env } from "process";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const PRIVATE_KEY = env.PRIVATE_KEY as string;
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "swisstronik",
+  solidity: "0.8.20",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: "",
+    customChains: [
+      {
+        network: "swisstronikTestnet",
+        chainId: 1291,
+        urls: {
+          apiURL: "https://explorer-evm.testnet.swisstronik.com/api",
+          browserURL: "https://explorer-evm.testnet.swisstronik.com",
+        },
+      },
+    ],
+  },
+};
+
+export default config;
